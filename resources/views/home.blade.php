5b22cb8cf5cc50c921805bb51962910112a133cc
PANDITHAjr <rcarballo.carlos@gmail.com>
2022-11-18 02:44:48 -0400

    restrinciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,13 +11,15 @@
                     <br>
                     <div class="col s12 m4">
                         <div class="card">
-                            <span  style="color: black"  class="card-title">FACULTAD</span>
+                            <span style="color: black" class="card-title">FACULTAD</span>
                             <div class="card-image">
-                                <img src="https://plus.unsplash.com/premium_photo-1661284857549-21bba6eda0b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2N0b2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60">
+                                <img
+                                    src="https://plus.unsplash.com/premium_photo-1661284857549-21bba6eda0b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2N0b2t8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60">
 
                             </div>
                             <div class="card-action">
-                                <a href="{{ route('producto.index') }}", class="btn btn-floating red accent-4 pulse"><i class="material-icons">account_balance</i></a>
+                                <a href="{{ route('producto.index') }}", class="btn btn-floating red accent-4 pulse"><i
+                                        class="material-icons">account_balance</i></a>
                                 <a style="color:brown; font-size: 14px; font-family: Georgia;">SCTOK</a>
                             </div>
                         </div>
@@ -26,36 +28,46 @@
                     <br>
                     <br>
                     <br>
-                    <div class="col s12 m4">
-                        <div class="card">
-                            <span style="color: black" class="card-title">INTEGRAL</span>
-                            <div class="card-image">
-                                <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80">
+                    @if (auth()->user()->personal->Tipo_Personal->descripcion == 'Administrador' or
+                        auth()->user()->personal->Tipo_Personal->descripcion == 'Entradas')
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <span style="color: black" class="card-title">INTEGRAL</span>
+                                <div class="card-image">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80">
 
-                            </div>
-                            <div class="card-action">
-                                <a href="#", class="btn btn-floating indigo darken-4 pulse"><i class="material-icons">assignment_returned</i></a>
-                                <a style="color:#d50000; font-size: 14px; font-family: Georgia;">RESG. ENTRADAS</a>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#", class="btn btn-floating indigo darken-4 pulse"><i
+                                            class="material-icons">assignment_returned</i></a>
+                                    <a style="color:#d50000; font-size: 14px; font-family: Georgia;">RESG. ENTRADAS</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                     <br>
                     <br>
                     <br>
                     <br>
-                    <div class="col s12 m4">
-                        <div class="card">
-                            <span style="color: black"  class="card-title">DEL CHACO</span>
-                            <div class="card-image">
-                                <img src="https://images.unsplash.com/photo-1620388640785-892616248ec8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aW52ZW50YXJpbyUyMGVudHJhZGFzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60">
+                    @if (auth()->user()->personal->Tipo_Personal->descripcion == 'Administrador' or
+                        auth()->user()->personal->Tipo_Personal->descripcion == 'Salidas')
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <span style="color: black" class="card-title">DEL CHACO</span>
+                                <div class="card-image">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1620388640785-892616248ec8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aW52ZW50YXJpbyUyMGVudHJhZGFzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60">
 
-                            </div>
-                            <div class="card-action">
-                                <a href="#", class="btn btn-floating red accent-4 pulse"><i class="material-icons">forward</i></a>
-                                <a style="color:brown; font-size: 14px; font-family: Georgia;"> RESG. SALIDAS</a>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#", class="btn btn-floating red accent-4 pulse"><i
+                                            class="material-icons">forward</i></a>
+                                    <a style="color:brown; font-size: 14px; font-family: Georgia;"> RESG. SALIDAS</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 </div>
 
             </div>
--- resources/views/layouts/menu-sidenav.blade.php
+++ resources/views/layouts/menu-sidenav.blade.php
@@ -1,27 +1,37 @@
-<ul id="sidenav-left" class="sidenav indigo black">
-    <li>
-        <div class="user-view center">
-            <div class="background">
-                <img src=https://images.unsplash.com/photo-1506220926022-cc5c12acdb35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5lZ3JvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                alt=""
-                class="responsive-img">
-            </div>
-
-            <div class="row">
-
-                <div class="col s9 push-s3 center-align">
-                    <a href="#user" class="centrado"><img class="circle responsive-img" src="{{ asset('https://linea4angol.cl/wp-content/uploads/2018/09/hombre.jpg') }}"></a>
+@if (auth()->user()->personal->Tipo_Personal->descripcion == 'Administrador')
+    <ul id="sidenav-left" class="sidenav indigo black">
+        <li>
+            <div class="user-view center">
+                <div class="background">
+                    <img src=https://images.unsplash.com/photo-1506220926022-cc5c12acdb35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5lZ3JvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                        alt="" class="responsive-img">
                 </div>
+
+                <div class="row">
+
+                    <div class="col s9 push-s3 center-align">
+                        <a href="#user" class="centrado"><img class="circle responsive-img"
+                                src="{{ asset('https://linea4angol.cl/wp-content/uploads/2018/09/hombre.jpg') }}"></a>
+                    </div>
+                </div>
+                <a href="#name"><span
+                        class="white-text name">{{ auth()->user()->personal->nombre . ' ' . auth()->user()->personal->apellido }}</span></a>
+                <a href="#email"><span
+                        class="white-text email">{{ auth()->user()->personal->Tipo_Personal->descripcion }}</span></a>
             </div>
-            <a href="#name"><span class="white-text name">{{ auth()->user()->personal->nombre.' '.auth()->user()->personal->apellido }}</span></a>
-            <a href="#email"><span class="white-text email">{{ auth()->user()->personal->Tipo_Personal->descripcion }}</span></a>
-        </div>
-    </li>
-    <li><a  class="subheader black-text">Administración:</a></li>
-    <li><a  style="color: #d50000"class="waves-effect" href="{{ route('personal.index') }}">Gestionar Personas<i class="material-icons" style="color: #1a237e">class</i></a></li>
-    <li><a  style="color: #d50000" class="waves-effect" href="{{ route('tipo_personal.index') }}">Gestionar Tipo Personal<i class="material-icons" style="color: #1a237e">class</i></a></li>
-    <li><a  style="color: #d50000" class="waves-effect" href="{{ route('usuario.index') }}">Gestionar Usuarios<i class="material-icons" style="color: #1a237e">class</i></a></li>
-    <li><a  style="color: #d50000" class="waves-effect" href="{{ route('producto.index') }}">Gestionar Almacen<i class="material-icons" style="color: #1a237e">class</i></a></li>
-    <li><a  style="color: #d50000" class="waves-effect" href="{{ route('tipo_producto.index') }}">Gestionar Tipo Productos<i class="material-icons" style="color: #1a237e">class</i></a></li>
-    <li><a  style="color: #d50000" class="waves-effect" href="{{ route('departamento.index') }}">Gestionar Departamento<i class="material-icons" style="color: #1a237e">class</i></a></li>
-</ul>
+        </li>
+        <li><a class="subheader black-text">Administración:</a></li>
+        <li><a style="color: #d50000"class="waves-effect" href="{{ route('personal.index') }}">Gestionar Personas<i
+                    class="material-icons" style="color: #1a237e">class</i></a></li>
+        <li><a style="color: #d50000" class="waves-effect" href="{{ route('tipo_personal.index') }}">Gestionar Tipo
+                Personal<i class="material-icons" style="color: #1a237e">class</i></a></li>
+        <li><a style="color: #d50000" class="waves-effect" href="{{ route('usuario.index') }}">Gestionar Usuarios<i
+                    class="material-icons" style="color: #1a237e">class</i></a></li>
+        <li><a style="color: #d50000" class="waves-effect" href="{{ route('producto.index') }}">Gestionar Almacen<i
+                    class="material-icons" style="color: #1a237e">class</i></a></li>
+        <li><a style="color: #d50000" class="waves-effect" href="{{ route('tipo_producto.index') }}">Gestionar Tipo
+                Productos<i class="material-icons" style="color: #1a237e">class</i></a></li>
+        <li><a style="color: #d50000" class="waves-effect" href="{{ route('departamento.index') }}">Gestionar
+                Departamento<i class="material-icons" style="color: #1a237e">class</i></a></li>
+    </ul>
+@endif
